72fd843909965ed842f1b95a7afa367f7b8291b4
snaens <samuelpelz007@gmail.com>
2023-04-23 23:16:34 +0200

    Massively touch up texts, update Box image, crop team image, add links, actually add timeline to gallery, remove copyright, add javascript notice

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ let full3 = false;
 
 function unfoldRobotZyklop() {
     if (full === false) {
-        document.getElementById('robot-zyklop-unfolded').innerHTML = '<img class="foto" src="assets/images/zyklop.jpg" alt="" width="700" height="450"><p class="description">This is our old robot: Zyklop. This was our first real experience with ROS and industrial standard robots.</p>';
+        document.getElementById('robot-zyklop-unfolded').innerHTML = '<img class="foto" src="assets/images/zyklop.jpg" alt="" width="700" height="450"><p class="description">This is Zyklop, our initial robot. It was our first real experience with ROS and professional robotics.<br><br>Zyklop is great at navigating rough terrain but has a deficit of sensors.</p>';
         document.getElementById('robot-zyklop').innerHTML = '';
         full = true;
     } else {
         document.getElementById('robot-zyklop-unfolded').innerHTML = '';
-        document.getElementById('robot-zyklop').innerHTML = '<p>The "Zyklop" is our old robot</p><p class="tinted">(click to read more)</p>';
+        document.getElementById('robot-zyklop').innerHTML = '<p>Zyklop is our first robot</p><p class="tinted">(click to read more)</p>';
         full = false;
     }
 }
 
 function unfoldRobotBento() {
     if (full1 === false) {
-        document.getElementById('robot-bento_box-unfolded').innerHTML = '<p class="description">The Bento Box is our newest addition to our robot family. It will be used in Bordeaux. </p><img class="foto" src="assets/images/bento-box_concept.png" alt="bento_bot_chassis" width="700" height="450">';
+        document.getElementById('robot-bento_box-unfolded').innerHTML = '<p class="description">Box is the newest addition to our robot family. It will be used in Bordeaux.<br><br>As you can see, Box is still in the design phase, but preparations (and purchases) are being made to begin assembly.<br><br>We chose the name mostly so that we can call it the bento \'Box\', but also because we presume it\'ll end up looking like a box, since this happened with Zyklop.</p><img class="foto" src="assets/images/bento-box_concept.png" alt="bento_bot_chassis" width="700" height="450">';
         document.getElementById('robot-bento_box').innerHTML = '';
         full1 = true;
     } else {
         document.getElementById('robot-bento_box-unfolded').innerHTML = '';
-        document.getElementById('robot-bento_box').innerHTML = '<p>The "Bento Box" is our next-gen robot</p><p class="tinted">(click to read more)</p>';
+        document.getElementById('robot-bento_box').innerHTML = '<p>Bento "Box" is our next-gen robot</p><p class="tinted">(click to read more)</p>';
         full1 = false;
     }
 }
 
 function unfoldTeam() {
     if (full2 === false) {
-        document.getElementById('team-unfolded').innerHTML = '<img class="foto" src="assets/images/the_team.jpg" alt="" width="700" height="450"><p class="description">Our team consists of a total of six members. Together they are able to efficiently work together, since everyone brings different skills to the table. <br> By participating in an international competition we want to show our strengths and obtain more experience.<p>';
+        document.getElementById('team-unfolded').innerHTML = '<img class="foto" src="assets/images/the_team.jpg" alt="" width="700" height="450"><p class="description">Our team consists of a total of six members. Together we are capable of great things since everyone adds their unique skill set to the team.<br><br>By participating in an international competition we strive to gain more experience and show our strengths.<p>';
         document.getElementById('team').innerHTML = '';
         full2 = true;
     } else {
@@ -41,12 +41,12 @@ function unfoldTeam() {
 
 function unfoldGallery() {
     if (full3 === false) {
-        document.getElementById('gallery-unfolded').innerHTML = '<video src="https://files.pipeman.org/robot/Roboter-Film-2.mp4" controls width="550" height="300"></video>';
+        document.getElementById('gallery-unfolded').innerHTML = '<div><video src="https://files.pipeman.org/robot/Roboter-Film-2.mp4" controls width="550" height="300"></video></div><div><img src="assets/images/mermaid-diagram-2023-03-08-150002.svg" alt="" height="900"></div>';
         document.getElementById('gallery').innerHTML = '';
         full3 = true;
     } else {
         document.getElementById('gallery-unfolded').innerHTML = ''
-        document.getElementById('gallery').innerHTML = '<p>You can see at what we\'ve been up to here</p><p class="tinted">(click to read more)</p>';
+        document.getElementById('gallery').innerHTML = '<p>See at what we\'ve been up to</p><p class="tinted">(click to read more)</p>';
         full3 = false;
     }
 }
